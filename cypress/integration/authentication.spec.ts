afa1ca888da0cdaf934d7f099c2d47def37a7c33
unknown <sbogart19@gmail.com>
2020-09-29 22:12:22 -0700

    Add addl Auth tests to Cypress

--- cypress/integration/authentication.spec.ts
+++ cypress/integration/authentication.spec.ts
@@ -1,6 +1,8 @@
+import { BlockedPage } from '../pages/BlockedPage';
 import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
 
+const blockedPage = new BlockedPage();
 const loginPage = new LoginPage();
 const homePage = new HomePage();
 
@@ -19,5 +21,30 @@ context('Authentication', () => {
     loginPage.inputLogin(validUserName, validPassword)
     cy.get(loginPage.submitButton).click()
     cy.get(homePage.userNameDisplay).should('contain', `Welcome ${userName}`)
+    })
+
+    it('will display an error when user attempts to login with incorrect credentials', () => {
+      const validUserName = "john"
+      const invalidUserName = "asdf"
+      const invalidPassword = "jkl"
+
+      loginPage.inputLogin(validUserName, invalidPassword)
+      cy.get(loginPage.submitButton).click()
+      cy.get(loginPage.errorHeader).should('exist')
+      .should('contain', "Error!")
+      .get(loginPage.errorText).should('contain', 'The username and password could not be verified.')
+
+      loginPage.inputLogin(invalidUserName, invalidPassword)
+      cy.get(loginPage.submitButton).click()
+      cy.get(loginPage.errorHeader).should('exist')
+      .should('contain', "Error!")
+      .get(loginPage.errorText).should('contain', 'The username and password could not be verified.') 
+    })
+
+    it('will display a blocked page if user attempts to use sql injection on login fields', () => {
+      const sqlInjection = "and 1=2"
+      loginPage.inputLogin(sqlInjection, sqlInjection)
+      cy.get(loginPage.submitButton).click()
+        .get(blockedPage.pageHeader).should('contain', blockedPage.headerText)
     })
 }) 
---
+++ cypress/pages/BlockedPage.ts
@@ -0,0 +1,6 @@
+import { Selector } from 'testcafe'
+
+export class BlockedPage {
+    pageHeader = '#cf-error-details h1'
+    headerText = "Sorry, you have been blocked"
+}
